239ea10e04243477dca5f0a921617899ae5a8fa4
codeconcept <contact@codeconcept.fr>
2019-06-09 18:22:17 +0200

    select element for picking number of days training per week

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+  import TrainingDay from "./components/TrainingDay.svelte";
+  export let name;
+</script>
+
+<style>
+  h1 {
+    color: purple;
+  }
+</style>
+
+<h1>{name}</h1>
+<TrainingDay/>
---
+++ src/components/TrainingDay.svelte
@@ -0,0 +1,37 @@
+<script>
+  const nbOfDays = [
+    { id: 1, text: "One day", value: 1 },
+    { id: 2, text: "Two days", value: 2 },
+    { id: 3, text: "Three days", value: 3 },
+    { id: 4, text: "Four day", value: 4 },
+    { id: 5, text: "Five days", value: 5 },
+    { id: 6, text: "Six days", value: 6 },
+    { id: 7, text: "one day", value: 7 }
+  ];
+
+  let nbDaysTrainingPerWeek;
+
+  function handleSubmit(event) {
+    console.log('handleSubmit');
+  }
+
+  function handleNumberDays(event) {
+    nbDaysTrainingPerWeek = event.target.value;
+    console.log(nbDaysTrainingPerWeek.value);
+  }
+</script>
+
+<style>
+
+</style>
+
+<div>
+  <form on:submit|preventDefault={handleSubmit}>
+    <select bind:value={nbDaysTrainingPerWeek}>
+      {#each nbOfDays as day (day.id)}
+        <option value={day}> {day.text} </option>
+      {/each}
+    </select>
+  </form>
+  {JSON.stringify(nbDaysTrainingPerWeek)}
+</div>
